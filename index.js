ebc2600041359ad4dac787030cf859534d7506b2
minecubeks <58666857+minecubeks@users.noreply.github.com>
2020-09-15 19:31:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,33 @@ bot.on("ready", () =>{
     bot.user.setActivity('minemen.serv.nu | -help', { type: 'LISTENING'}).catch(console.error);
 });
 
+bot.on('message', message =>{
+ 
+    let args = message.content.substring(PREFIX.length).split(' ')
+ 
+    switch(args[0]){
+        case 'mc':
+ 
+            if(!args[1]) return message.channel.send('You must type a minecraft server ip')
+            if(!args[2]) return message.channel.send('You must type a minecraft server port')
+ 
+            ping(args[1], parseInt(args[2]), (error, reponse) =>{
+                if(error) throw error
+                const Embed = new RichEmbed()
+                .setTitle('Server Status')
+                .addField('Server IP', reponse.host)
+                .addField('Server Version', reponse.version)
+                .addField('Online Players', reponse.onlinePlayers)
+                .addField('Max Players', reponse.maxPlayers)
+                
+                message.channel.send(Embed)
+            })
+        break
+ 
+    }
+ 
+})
+
 bot.on('message', message => {
 	if (message.content === `${prefix}ip`) {
         message.channel.send('IP serveru je: **minemen.serv.nu**');
